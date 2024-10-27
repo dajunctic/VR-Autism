33ec6a50b28519e77e22021c8c8e8e1f3130e55f
Trần Thị Trà Giang <54837897+kami2608@users.noreply.github.com>
2024-10-27 23:39:03 +0700

    update menu

--- Assets/Dang/Scripts/LessonDetailController.cs
+++ Assets/Dang/Scripts/LessonDetailController.cs
@@ -5,26 +5,28 @@ using TMPro;
 
 public class LessonDetailController : MonoBehaviour
 {
-    public GameObject lessonDetailPanel; 
+    public GameObject lessonDetailPanel;
+    public GameObject chooseTypePanel;
+    public ChooseTypeController chooseTypeController;
     public TextMeshProUGUI titleText; 
-    public TextMeshProUGUI descriptionText; 
+    public TextMeshProUGUI descriptionText;
+    public Image coverImage;
     public Button closeButton;
     public Button playButton;
-    public string sceneName;
 
 
     void Start()
     {
         closeButton.onClick.AddListener(HideLessonDetailPanel);
-        playButton.onClick.AddListener(() => LoadLessonScene(sceneName));
-        lessonDetailPanel.SetActive(false); 
+        playButton.onClick.AddListener(() => ProceedToNextStep());
     }
 
 
-    public void ShowLessonDetailPanel(string title, string description)
+    public void ShowLessonDetailPanel(string title, string description, Sprite cover)
     {
         titleText.text = title;
         descriptionText.text = description;
+        coverImage.sprite = cover;
         lessonDetailPanel.SetActive(true);
     }
 
@@ -34,9 +36,11 @@ public class LessonDetailController : MonoBehaviour
         lessonDetailPanel.SetActive(false);
     }
 
-    public void LoadLessonScene(string sceneName)
+    private void ProceedToNextStep()
     {
-        SceneManager.LoadScene(sceneName);
+        chooseTypeController.ShowChooseTypePanel(titleText.text);
+        lessonDetailPanel.SetActive(false);
+        chooseTypePanel.SetActive(true);
     }
 
 }
